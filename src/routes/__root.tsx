ad30a4ac14fb86c022eff220b7ceb011ce5a2e20
danielcech933-hue <danielcech933@gmail.com>
2026-08-14 14:58:40 +0200

    feat: enforce verified phone access

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -31,6 +31,7 @@ import { AuthProvider, useAuth } from "@/lib/auth";
 import { Avatar } from "@/lib/avatars";
 import { NotificationsBell } from "@/lib/notifications";
 import { DmProvider, DmBell } from "@/lib/dm";
+import { PhoneVerificationGate } from "@/components/PhoneVerificationGate";
 
 import { SiteFooter } from "@/components/SiteFooter";
 import { FloatingNav } from "@/components/FloatingNav";
@@ -128,7 +129,7 @@ function RootComponent() {
             <div className="flex min-h-screen flex-col">
               <StadiumBackdrop />
               <SiteHeader />
-              <div className="flex-1"><PageTransition /></div>
+              <div className="flex-1"><PhoneVerificationGate><PageTransition /></PhoneVerificationGate></div>
               <SiteFooter />
               <FloatingNav />
               <WinCelebrations />
